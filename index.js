310b8f13d4f16a3b83bf00214de85919a100de9d
Mariel Juliene Macabudbud <arkadx213@gmail.com>
2025-04-18 10:58:29 +0800

    adding the frontend app

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist"));
+
 const requestLogger = (request, response, next) => {
 	console.log("Method:", request.method);
 	console.log("Path:", request.path);
